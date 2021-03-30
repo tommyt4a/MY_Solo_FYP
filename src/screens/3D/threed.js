583bdc8e2879e753dce65b98fa7a0d550b174cb7
AAA <1155110781@link.cuhk.edu.hk>
2021-03-30 17:21:36 +0800

    成功第一步

--- src/screens/3D/threed.js
+++ src/screens/3D/threed.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {View} from 'react-native'
+import { Renderer } from 'expo-three';
+import { THREE } from 'expo-three';
+import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
+
+global.THREE = global.THREE || THREE;
 
 class threed extends React.Component{
-    render(){
-        return(
-            <View></View>
-        )
-    }
+    render() {
+        return (
+            <GLView
+            style={{ flex: 1 }}
+            
+            
+          />
+        );
+      }
 }
 
-export default threed
+
+export default threed;
